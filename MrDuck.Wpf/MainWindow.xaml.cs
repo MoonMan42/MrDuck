84f35d2143038bccc073872bd94852b9c8fc0972
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2021-03-30 19:29:28 -0400

    code clean up

--- MrDuck.Wpf/MainWindow.xaml.cs
+++ MrDuck.Wpf/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using MrDuck.Services;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Media;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace MrDuck.Wpf
@@ -72,7 +61,7 @@ namespace MrDuck.Wpf
             MouseService.MoveMouse(gen.Next(-25, 25), gen.Next(-25, 25));
         }
 
-       
+
 
         private void PlayQuack()
         {
